adcdde1da52f9a4aba8ac8d45e3d17b36c1109fb
Kyle Kemp <kyle@seiyria.com>
2016-03-18 11:42:52 -0500

    adding customized icons for reactive-retro

---
+++ .icon-map.js
@@ -0,0 +1,52 @@
+module.exports = {
+
+  // global icons
+  "health-normal": "global-hp",
+  "drop": "global-mp",
+  "coins": "global-money",
+
+  // item icons
+  "round-star": "item-tier-1",
+  "seven-pointed-star": "item-tier-2",
+  "stars-stack": "item-tier-3",
+
+  // gear icons
+  "back-forth": "gear-swap",
+
+  // skill display
+  "target-dummy": "skill-target",
+  "sands-of-time": "skill-cooldown",
+
+  // extra-icons
+  "crosshair": "extra-gps",
+
+  // menu icons
+  "anatomy": "menu-player",
+  "world": "menu-world",
+  "swap-bag": "menu-inventory",
+  "bowman": "menu-skills",
+  "juggler": "menu-traits",
+  "dark-squad": "menu-party",
+  "cog": "menu-options",
+  "entry-door": "menu-logout",
+
+  // inventory menu icons
+  "broadsword": "inventory-menu-weapon",
+  "armor-vest": "inventory-menu-armor",
+  "potion-ball": "inventory-menu-item",
+
+  // status effects
+  "health-increase": "status-regenerate",
+  "cloak-dagger": "status-stealth",
+  "blindfold": "status-blind",
+  "fire": "status-burn",
+  "frozen-orb": "status-freeze",
+  "electric": "status-shock",
+  "mace-head": "status-stun",
+  "targeting": "status-acc-plus",
+  "target-arrows": "status-dex-plus",
+  "clover-2": "status-luk-plus",
+  "magic-lamp": "status-mnt-plus",
+  "strong": "status-str-plus",
+  "nested-hearts": "status-vit-plus"
+};
--- gulpfile.js
+++ gulpfile.js
@@ -1,9 +1,11 @@
 
 const fs = require('fs');
+const path = require('path');
 const minimatch = require('minimatch');
 
 const gulp = require('gulp');
 const unzip = require('gulp-unzip');
+const filter = require('gulp-filter');
 const rename = require('gulp-rename');
 const download = require('gulp-download');
 const iconfont = require('gulp-iconfont');
@@ -14,21 +16,34 @@ const ghPages = require('gulp-gh-pages');
 const BASE_URL = 'http://game-icons.net/archives/svg/zip/000000/transparent/game-icons.net.svg.zip';
 const FONT_NAME = 'game-icons';
 
+const ONLY_ICONS = require('./.icon-map');
+
 gulp.task('build:font', () => {
     const fileCounts = {};
+    const includePaths = {};
 
     return download(BASE_URL)
         .pipe(unzip({
           filter: (entry) => minimatch(entry.path, '**/*.svg')
         }))
-        .pipe(rename(path => {
-          if(fileCounts[path.basename]) {
-            fileCounts[path.basename] += 1;
-            path.basename = `${path.basename}-${fileCounts[path.basename]}`;
+        .pipe(rename(filePath => {
+          if(fileCounts[filePath.basename]) {
+            fileCounts[filePath.basename] += 1;
+            filePath.basename = `${filePath.basename}-${fileCounts[filePath.basename]}`;
           } else {
-            fileCounts[path.basename] = 1;
+            fileCounts[filePath.basename] = 1;
           }
-          return path;
+
+          if(ONLY_ICONS[filePath.basename]) {
+            filePath.basename = ONLY_ICONS[filePath.basename];
+            includePaths[`${filePath.dirname}/${filePath.basename}${filePath.extname}`] = true;
+          }
+
+          return filePath;
+        }))
+        .pipe(filter(filePath => {
+          const pathData = path.parse(filePath.path);
+          return includePaths[`${pathData.dir}/${pathData.base}`];
         }))
         .pipe(iconfontCss({
             fontName: FONT_NAME,
